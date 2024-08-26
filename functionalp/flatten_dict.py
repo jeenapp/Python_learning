983f867183888f1a196b4829be385d5c2a85c11b
Jeena <33783425+jeenapp@users.noreply.github.com>
2024-08-26 17:42:38 +0530

    Add files via upload

---
+++ functionalp/flatten_dict.py
@@ -0,0 +1,12 @@
+def flatten_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+nested_dict = {'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4}
+print(flatten_dict(nested_dict)) 
